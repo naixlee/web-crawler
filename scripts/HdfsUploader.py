a6cacb0ea5b14fee3cdf4240f4970971de97212c
Xian Li <xli1@linkedin.com>
2015-08-24 15:49:49 -0700

    fix bug in hdfs uploader

--- scripts/HdfsUploader.py
+++ scripts/HdfsUploader.py
@@ -19,16 +19,17 @@ output_file = args.output
 seed_file = args.seed
 
 seed_file_handler = open(seed_file, 'r')
-seed_content = seed_file_handler.readline()
+seed_content = seed_file_handler.readlines()
 company_info = {}
+
 for line in seed_content:
-	strs = line.split('\t')
-	if len(strs) < 3:
-		continue
-	company_id = strs[1]
-	company_name = strs[2]
-	company_cik = strs[0]
-	company_info[company_id] = company_name + '@' + company_cik
+    strs = line.split('\t')
+    if len(strs) < 3:
+        continue
+    company_id = strs[1]
+    company_name = strs[2]
+    company_cik = strs[0]
+    company_info[company_id] = company_name + '@' + company_cik
 seed_file_handler.close()
 
 schema = avro.schema.parse(open(schema_file).read())
@@ -37,28 +38,26 @@ writer = DataFileWriter(open(output_file, 'w'), DatumWriter(), schema)
 dirs = os.listdir(input_dir)
 
 for subdir in dirs:
-	print subdir
-	if os.path.isdir(input_dir + '/' + subdir):
-		print 'dir: ', subdir
-		for f in os.listdir(input_dir + '/' + subdir):
-			print f
-			if os.path.isfile(input_dir + '/' + subdir + '/' + f) and f.endswith('.html'):
-				item = {}
-				content = codecs.open(input_dir + '/' + subdir + '/' + f, 'r', 'utf-8')
-				company_id = subdir
+    if os.path.isdir(input_dir + '/' + subdir):
+        for f in os.listdir(input_dir + '/' + subdir):
+            if os.path.isfile(input_dir + '/' + subdir + '/' + f) and f.endswith('.html'):
+                item = {}
+                content = codecs.open(input_dir + '/' + subdir + '/' + f, 'r', 'utf-8')
+                company_id = subdir
 
-				temp = f[0: f.find('.html')].split('-')
-				year = temp[0]
-				quarter = temp[1]
+                temp = f[0: f.find('.html')].split('-')
+                year = temp[0]
+                quarter = temp[1]
 
-				item['li_company_id'] = company_id
-				item['filing_year'] = year
-				item['filing_quarter'] = quarter
+                item['li_company_id'] = company_id
+                item['filing_year'] = year
+                item['filing_quarter'] = quarter
 
-				if company_info.has_key(company_id):
-					info = company_info[company_id].split('@')
-					item['name'] = info[0]
-					item['cik'] = info[1]
-				item['page_source'] = content.read()
-				writer.append(item)
+                if company_info.has_key(company_id):
+                    info = company_info[company_id].split('@')
+                    item['name'] = info[0]
+                    item['cik'] = info[1]
+                item['page_source'] = content.read()
+                print item['li_company_id'], item['name'], item['cik'], item['filing_year'], item['filing_quarter']
+                writer.append(item)
 writer.close()
